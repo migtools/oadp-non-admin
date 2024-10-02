11562cb58e6e58e5e05bfaa6c6fb0ff22a99f2c9
Mateus Oliveira <msouzaol@redhat.com>
2024-10-02 14:14:50 -0300

    fix: Change project domain
    Signed-off-by: Mateus Oliveira <msouzaol@redhat.com>

--- internal/controller/nonadminbackup_controller.go
+++ internal/controller/nonadminbackup_controller.go
@@ -55,9 +55,9 @@ const (
 	conditionUpdateError   = "Failed to update NonAdminBackup Condition"
 )
 
-// +kubebuilder:rbac:groups=nac.oadp.openshift.io,resources=nonadminbackups,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=nac.oadp.openshift.io,resources=nonadminbackups/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=nac.oadp.openshift.io,resources=nonadminbackups/finalizers,verbs=update
+// +kubebuilder:rbac:groups=oadp.openshift.io,resources=nonadminbackups,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=oadp.openshift.io,resources=nonadminbackups/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=oadp.openshift.io,resources=nonadminbackups/finalizers,verbs=update
 
 // +kubebuilder:rbac:groups=velero.io,resources=backups,verbs=get;list;watch;create;update;patch
 
