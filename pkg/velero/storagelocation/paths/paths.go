decb4ec92c9fe4527337dad9a32607274a21f178
Tiger Kaovilai <tkaovila@redhat.com>
2025-02-06 01:06:49 -0500

    lint
    Signed-off-by: Tiger Kaovilai <tkaovila@redhat.com>

--- pkg/velero/storagelocation/paths/paths.go
+++ pkg/velero/storagelocation/paths/paths.go
@@ -1,23 +1,21 @@
-// Copyright 2024.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// Copyright 2024.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//	http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+Copyright 2024.
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package paths  helps NAC controllers with getting relevant object store paths of interests
+// generally as defined in https://github.com/vmware-tanzu/velero/blob/0a280e57863c70495a2dfb65787615a68a0e7b03/pkg/persistence/object_store_layout.go
 package paths
 
 import (
@@ -32,18 +30,18 @@ import (
 
 // BasePathForLocation returns base path for a given storage location
 // <protocol>://<bucket>/<prefix>
-func BasePathForLocation(c client.Client, storageLocation *velerov1.BackupStorageLocation) (basepath string, err error) {
+func BasePathForLocation(storageLocation *velerov1.BackupStorageLocation) (basepath string, err error) {
 	var protocol string
 	switch storageLocation.Spec.Provider {
 	case "aws":
 		protocol = "s3://"
 	// TODO: implement azure
-	// azure format would be source-uri, like https://srcaccount.file.core.windows.net/myshare/mydir...
+	// azure format would be source-uri, like https://srcaccount.file.core.windows.net/myshare/mydir... //nolint:dupword // ehh
 	// see: https://learn.microsoft.com/en-us/cli/azure/storage/file/copy?view=azure-cli-latest#az-storage-file-copy-start
 	// TODO: implement gcp
-	// gcp format would be gs://my-bucket, see: https://cloud.google.com/sdk/gcloud/reference/storage/cp
+	//nolint:dupword // gcp format would be gs://my-bucket, see: https://cloud.google.com/sdk/gcloud/reference/storage/cp
 	default:
-		return "", errors.New("unimplemented provider") //nolint:revive // empty return
+		return "", errors.New("unimplemented provider")
 	}
 	return path.Join(protocol, storageLocation.Spec.ObjectStorage.Bucket, storageLocation.Spec.ObjectStorage.Prefix), nil
 }
@@ -55,9 +53,9 @@ func BasePathForBackup(c client.Client, backup *velerov1.Backup) (basepath strin
 	storageLocation := velerov1.BackupStorageLocation{}
 	err = c.Get(context.Background(), client.ObjectKey{Name: storageLocationName, Namespace: backup.Namespace}, &storageLocation)
 	if err != nil {
-		return "", errors.Join(errors.New("unable to get base path for backup"), err) //nolint:revive // empty return
+		return "", errors.Join(errors.New("unable to get base path for backup"), err)
 	}
-	return BasePathForLocation(c, &storageLocation)
+	return BasePathForLocation(&storageLocation)
 }
 
 // BasePathForRestore returns base path for a given restore
@@ -77,7 +75,7 @@ func BasePathForRestore(c client.Client, restore *velerov1.Restore) (basepath st
 func BackupLogs(c client.Client, backup *velerov1.Backup) (string, error) {
 	basepath, err := BasePathForBackup(c, backup)
 	if err != nil {
-		return "", err //nolint:revive // empty return
+		return "", err
 	}
 	return path.Join(basepath, "backups", backup.Name, fmt.Sprintf("%s-logs.gz", backup.Name)), nil
 }
@@ -86,7 +84,7 @@ func BackupLogs(c client.Client, backup *velerov1.Backup) (string, error) {
 func RestoreLogs(c client.Client, restore *velerov1.Restore) (string, error) {
 	basepath, err := BasePathForRestore(c, restore)
 	if err != nil {
-		return "", err //nolint:revive // empty return
+		return "", err
 	}
 	return path.Join(basepath, "restores", restore.Name, fmt.Sprintf("restore-%s-logs.gz", restore.Name)), nil
 }
@@ -96,7 +94,7 @@ func RestoreLogs(c client.Client, restore *velerov1.Restore) (string, error) {
 func BackupResourceList(c client.Client, backup *velerov1.Backup) (string, error) {
 	basepath, err := BasePathForBackup(c, backup)
 	if err != nil {
-		return "", err //nolint:revive // empty return
+		return "", err
 	}
 	return path.Join(basepath, "backups", backup.Name, fmt.Sprintf("%s-resource-list.json.gz", backup.Name)), nil
 }
@@ -105,7 +103,7 @@ func BackupResourceList(c client.Client, backup *velerov1.Backup) (string, error
 func RestoreResourceList(c client.Client, restore *velerov1.Restore) (string, error) {
 	basepath, err := BasePathForRestore(c, restore)
 	if err != nil {
-		return "", err //nolint:revive // empty return
+		return "", err
 	}
 	return path.Join(basepath, "restores", restore.Name, fmt.Sprintf("restore-%s-resource-list.json.gz", restore.Name)), nil
 }
