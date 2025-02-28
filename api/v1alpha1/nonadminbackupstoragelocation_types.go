bdb2859cccd8f69cff21f10b8ce5eee6b712567a
Michal Pryc <mpryc@redhat.com>
2025-02-28 11:58:25 +0100

    CRD updates for the oc CLI fields for the NAR, NAB and NABSL objects
      Implements #232 with additional fields for:
       - NonAdminBackup
       - NonAdminRestore
       - NonAdminBackupStorageLocation
    
    Signed-off-by: Michal Pryc <mpryc@redhat.com>

--- api/v1alpha1/nonadminbackupstoragelocation_types.go
+++ api/v1alpha1/nonadminbackupstoragelocation_types.go
@@ -71,6 +71,10 @@ type NonAdminBackupStorageLocationStatus struct {
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
 // +kubebuilder:resource:path=nonadminbackupstoragelocations,shortName=nabsl
+// +kubebuilder:printcolumn:name="Approved",type="string",JSONPath=".status.conditions[?(@.type=='ClusterAdminApproved')].status"
+// +kubebuilder:printcolumn:name="BSL-Status",type="string",JSONPath=".status.veleroBackupStorageLocation.status.phase"
+// +kubebuilder:printcolumn:name="Status",type="string",JSONPath=".status.phase"
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
 
 // NonAdminBackupStorageLocation is the Schema for the nonadminbackupstoragelocations API
 type NonAdminBackupStorageLocation struct {
